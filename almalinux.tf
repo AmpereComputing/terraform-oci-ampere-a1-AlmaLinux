be4cf11bb21563dec38f85195db5f3f4e2ed73cc
Peter Pouliot <peter@pouliot.net>
2022-03-08 11:31:15 -0500

    Update almalinux.tf

--- almalinux.tf
+++ almalinux.tf
@@ -1,11 +1,11 @@
-# This dynamically gets the Oracle Linux 8.4 images, the first result in the images 
+# This dynamically gets the AlmaLinux 8.4 images, the first result in the images 
 # list (index 0) is the latest patched version.  Filter to specific display name 
 # pattern to include the aarch64 images
 
-data "oci_core_images" "oraclelinux-8_4-aarch64" {
+data "oci_core_images" "almalinux-8_4-aarch64" {
   compartment_id = var.tenancy_ocid
 
-  operating_system         = "Oracle Linux"
+  operating_system         = "Alma Linux"
   operating_system_version = "8"
 
   # include Aarch64 specific images
@@ -16,10 +16,10 @@ data "oci_core_images" "oraclelinux-8_4-aarch64" {
   }
 }
 
-output "OracleLinux-8_4-aarch64-latest-name" {
-  value = data.oci_core_images.oraclelinux-8_4-aarch64.images.0.display_name
+output "AlmaLinux-8_4-aarch64-latest-name" {
+  value = data.oci_core_images.almalinux-8_4-aarch64.images.0.display_name
 }
 
-output "OracleLinux-8_4-aarch64-latest_ocid" {
-  value = data.oci_core_images.oraclelinux-8_4-aarch64.images.0.id
+output "AlmaLinux-8_4-aarch64-latest_ocid" {
+  value = data.oci_core_images.almalinux-8_4-aarch64.images.0.id
 }
